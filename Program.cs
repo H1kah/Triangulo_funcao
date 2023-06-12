a447a973a5d6a35e7ab23fea3980ebeff5511c20
H1karu <124624556+H1kah@users.noreply.github.com>
2023-06-12 20:59:18 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,68 +1,68 @@
-﻿//agora estamos usando o C#, a biblioteca é diferente
 using System;
-//o "System.Globalization é uma biblioteca para tudo. É algo mais completo, não precisa destacar tanto no começo do código.
 using System.Globalization;
+//como todo código, todo o arquivo precisa de uma biblioteca para que cada code seja compreendido, já que, sem eles, será apenas palavras sem signigicado nenhum.
 namespace Course
+//é um recurso que ajuda a organizar e evitar conflitos de nomes em um código
 {
-    //criando uma classe para que possa ser referida depois
     public class Triangulo
+    //Aqui está criando uma classe triângulo, sendo a fórmula que vai ser mencionada depois de forma simples.
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        //aqui são as variáveis que vão ser utilizadas para fazer a calculadora
+        public double CalcularArea()
         {
-        //aqui está criando a parte principal da programação, destacando os valores de cada coisa.
-        public static void Main(string[] args)
-        {
-            //está colocando os valores utilizando o double, já que é necessário mais espaço, diferente do float que é muito simples para se utilizar 
-            double[] medidasX = LerMedidasTriangulo("X");
-            double[] medidasY = LerMedidasTriangulo("Y");
-            //aqui está criando o array da área utilizando o double também
-            double areaX = CalcularAreaTriangulo(medidasX);
-            double areaY = CalcularAreaTriangulo(medidasY);
-            //Não estamos mais no C++, agora utilizamos o Console.WriteLine para que a mensagem apareça na tela.
-            //A mensagem está como double. O "To string´" está convertando para o string, deixando-o mais leve.
-            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-            //aqui é o mesmo do c++. O "if" é o "se". Ou seja, se áreaX for maior que áreaY, a resposta será uma. Se não (else), será outra
-            if (areaX > areaY)
-            {
-                //mensagem na tela caso a área X seja realmente maior
-                Console.WriteLine("Maior área: X");
-            }
-            //caso não seja:
-            else
-            {
-                //áreaY maior
-                Console.WriteLine("Maior área: Y");
-            }
+            double p = (A + B + C) / 2.0;
+            //a grandiosa fórmula, dando um valor para o p
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            //já sabendo o que é o p, agora vai calcular a área. Ou seja, primeiro o programa calcula o p e depois a área.
+            return area;
         }
-        //aqui é uma parte para decidir o peso das medidas
-        public static double[] LerMedidasTriangulo(string nomeTriangulo)
-        {
-            //está criando um novo array, tendo o peso de 3 valores, justo o necessário para calcular o triângulo
-            double[] medidas = new double[3];
-            //invés de ficar digitando várias vezes o código, só digita uma vez e coloca o "nome triângulo", que é uma resposta variável
-            Console.WriteLine("Entre com as medidas dos triângulos " + nomeTriangulo + ":");
-            //aqui está mostrando o peso necessário para rodar. No caso, só se utiizará 3 valores para cada triângulo
-            for (int i = 0; i < 3; i++)
-            {
-                medidas [i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            }
-            //aqui retornará os valores para medidas
-            return medidas;
-        }
-        //aqui está criando uma parte específica para entrar com as medidas, associando com os valores criados antes e a fórmula que aparecerá à seguir
-        public static double CalcularAreaTriangulo(double [] medidas)
-        {
-            //aqui está associando as novas variáveis que serão utilizadas para a fórmula, todas utilizando o double
-            double a = medidas[0];
-            double b = medidas[1];
-            double c = medidas[2];
-            //criando a fórmula que será utilizada
-            double p = (a + b + c) / 2.0;
-            //agora que utilizará meios que o programa em si não reconhece, o Math.Sqrt é colocado para que aí então o programa entenda.
-            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-            //retorna o valor pra área
-            return area;    
-        }
-            
+    }
+    public class Program
+        //aqui agora vai criar mais variáveis, sendo a variável X e Y, mostrando ao programa qeu será necessário fazer mais uma conta.
+    {
 
+    public static void Main(string[] args)
+    //Não é mais int main, e sim, void main. Nessa parte, está criando outro public, citando o peso do comando(string), etc.
+    {
+        Triangulo x = LerTriangulo("x");
+        Triangulo y = LerTriangulo("y");
+
+        //aqui está criando dois triângulos, associando com x e y.
+
+            double areaX = x.CalcularArea();
+            double areaY = y.CalcularArea();
+            //aqui está criando mais uma variável, sendo a área X e Y.
+
+            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            //mensagens, são elas que aparecerão na tela com o resultado
+            if (areaX > areaY)
+            //If, sendo justamente o que diz "se". Se a área X for maior...
+            {
+                Console.WriteLine("Maior área: X");
+                //...essa será a resposta, Maior área X
+            }
+            else
+            //Se não corresponder o requisito anterior, a resposta será o que está à seguir: Maior área: Y
+        {
+            Console.WriteLine("Maior área: Y");
+        }
+    }
+    public static Triangulo LerTriangulo(string nomeTriangulo)
+    {
+            //aqui é o public para a mensagem qeu vai aparecer na tela. Está sendo criada depois de todas as fórmulas e esquemais criados anteriormente
+        Triangulo triangulo = new Triangulo();
+
+        Console.WriteLine("Entre com as medidas do triângulo " + nomeTriangulo + ":");
+        triangulo.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        triangulo.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            triangulo.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //aqui são as questões que vão aparecer. Tudo que for digitado será associado com a, b e c. Para isso, vai precisar digitar o número, apertar enter, digitar o número e assim por diante
+
+            return triangulo;
+      }
     }
 }
